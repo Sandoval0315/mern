6a02398b0ce9d5231b541b7fb7cf3f4fb8d68bae
sandoval <20200300@ricaldone.edu.sv>
2025-03-03 15:19:00 -0600

    first commit

---
+++ backend/app.js
@@ -0,0 +1,8 @@
+//importamos todo de la libreria de express
+
+import express from "express";
+
+//creamos una constante a la libreria que importe y la ejecuto 
+const app = express();
+
+export default app; 
---
+++ backend/index.js
@@ -0,0 +1,13 @@
+//mandar a llamar al archivo app.js
+import app from "./app.js"
+
+//creamos una funcion para ejecutar el servidor
+
+async function main() {
+    const port = 4000;
+    app.listen(port);
+    console.log("servidor corriendo");
+}
+
+//encendemos el servidor
+main()
